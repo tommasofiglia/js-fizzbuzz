dd0076b49d12361f967e4ae60275ff98e243c6ec
Tommaso Figlia <tommasofiglia@gmail.com>
2020-11-19 16:42:31 +0100

    Esercizio completo con risultati stampati a schermo in una lista .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 //Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
+//Creo un array vuoto e lo riempio di numeri che vanno da uno a 100, sostituendo però ai multipli dei numeri richiesti dall'esercizio, le diciture "Fizz" , "Buzz" e "FizzBuzz" attraverso un ciclo for con la condizione if
+
 var numbers = [];
 for (var i = 1; i <= 100; i++) {
 
@@ -15,4 +17,11 @@ for (var i = 1; i <= 100; i++) {
 
 }
 
-console.log(numbers);
+//Creo un altro ciclo for, una volta che l'array è pieno dei suoi elementi descritti precedentemente, per fare in modo che ogni elemento sia stampato a schermo in una lista non ordinata
+
+for (var i = 0; i < numbers.length; i++) {
+  var numbersElement = numbers[i];
+
+  var numberList = document.getElementById('stampa-schermo').innerHTML;
+  document.getElementById('stampa-schermo').innerHTML = numberList + "<li>" + numbersElement + "</li>";
+}
